63a16974395e8f7fdf7dd5cbe0f373b3c40f69ca
emily baier <emilyrbaier@gmail.com>
2019-07-29 13:00:56 -0700

    fixed footer overlaying gif on small screens

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { GifWrapper } from '../styles/styles';
 
 function Gif({ imageURL, url, title }) {
   return (
-    <a href={url}><img src={imageURL} alt={title}/></a>
+    <GifWrapper>
+      <a href={url}><img src={imageURL} alt={title}/></a>
+    </GifWrapper>
   );
 }
 
